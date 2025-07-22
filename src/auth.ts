4d1762d993e9ff2de71199299b78f04ecb42f278
supanutOn <supanut.tan@ondemand.in.th>
2025-07-22 10:15:54 +0700

    feat: intigate with google login

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,29 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import { unauthorized } from 'next/navigation';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-  callbacks: {},
+  callbacks: {
+    async signIn({ account }) {
+      const response = await fetch(
+        `${process.env.ADMIN_PORTAL_API_BASE_URL}/api/auth/google-login`,
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ idToken: account?.id_token }),
+        }
+      );
+
+      if (response.status === 401) {
+        unauthorized();
+      }
+
+      if (!response.ok) {
+        return false;
+      }
+
+      return true;
+    },
+  },
 });
